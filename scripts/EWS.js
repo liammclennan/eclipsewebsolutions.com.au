098701d1dcd08f3ff9bcc1999c8ceb2257adbed0
Liam McLennan <liam.mclennan@gmail.com>
2012-03-18 08:19:01 +1000

    imported web site

---
+++ scripts/EWS.js
@@ -0,0 +1,34 @@
+
+var EWS = {};
+
+EWS.setupLinks = function() {
+
+	var dialog_options = {
+				bgiframe: true,
+				modal: true,
+				overlay: {
+					backgroundColor: '#000',
+					opacity: 0.9
+				},
+				width: 800,
+				autoOpen: false
+		};
+	
+	$('#creation_menu').bind('click', function() {
+		$('#creation_story').dialog(dialog_options);
+		$('#creation_story').dialog('open');
+	});
+	
+	$('#services_menu').bind('click', function() {
+		$('#services').dialog(dialog_options);
+		$('#services').dialog('open');
+	});
+	
+	$('#contact_menu,#cardfile').bind('click', function() {
+		$('#contact').dialog(dialog_options);
+		$('#contact').dialog('open');
+	});
+	
+	$('.menu_item').hover(function() { $(this).addClass('menu_hover'); }, function() {$(this).removeClass('menu_hover');});
+
+};
